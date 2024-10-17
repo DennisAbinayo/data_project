6ec091782610d9df46e9d1b592b84cb4f45edc51
DennisAbinayo <dennisabinayo5@gmail.com>
2024-10-17 17:14:45 +0300

    first commit

---
+++ values.py
@@ -0,0 +1,49 @@
+title_options = [
+    "", 
+    "Mr", 
+    "Mrs", 
+    "Ms", 
+    "Dr"
+]
+
+
+course_options = [
+    "",
+    "Data Analytic Lite Core",
+    "Data Science Core",
+    "Web Development Lite Core",
+    "Software Engineering Core",
+    "Product Management Course",
+    "Data Structures and Algorithms",
+    "30-day Data Storytelling Challenge (SQL and Power BI)",
+    "30-day Web Design Challenge (HTML, CSS, Figma UI)",
+]
+
+
+nationality_options = [
+    "Burundian",
+    "Djiboutian",
+    "Eritrean",
+    "Ethiopian",
+    "Kenyan",
+    "Rwandan",
+    "South Sudanese",
+    "Tanzanian",
+    "Ugandan",
+    "Somali"
+]
+
+
+column_widths = {
+    "ID": 50,
+    "Status": 100,
+    "Title": 70,
+    "First Name": 120,
+    "Last Name": 120,
+    "Email": 200,
+    "Course": 150,
+    "Modules": 80,
+    "Age": 60,
+    "Nationality": 120
+}
+
